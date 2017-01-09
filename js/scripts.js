106136af6a146e34311953f4819a9e93c9d1a093
Clayton Collins and Katharyn Reed <katharyn@>
2017-01-09 15:24:20 -0800

    add text button, css styles, various scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,63 @@
+//
+// var add = function(n1, n2) {alert(n1+n2)};
+// var sub = function(n1, n2) {alert(n1+n2)};
+// var mult = function(n1, n2) {alert(n1*n2)};
+// var three = function(n1, n2, n3) {alert(n1*n2*n3)};
+// var div = function(n1, n2) {alert(n1/n2)};
+// var remainder = function(n1,n2) {alert(n1%n2)};
+//
+// var number1 = parseInt(prompt("enter a number: "));
+// var number2 = parseInt(prompt("enter another: "));
+// var operator = prompt("add, subtract, multiply, or divide?")
+//
+// if (operator === "add") {
+//   add(number1, number2);
+// } else if (operator === "subtract") {
+//   sub(number1, number2);
+// } else if (operator === "multiply") {
+//   mult(number1, number2);
+// } else if (operator === "divide") {
+//  div(number1, number2);
+// } else {
+//   alert("You suck, try again scrub.")
+// }
 
-var add = function(n1, n2) {alert(n1+n2)};
-var sub = function(n1, n2) {alert(n1+n2)};
-var mult = function(n1, n2) {alert(n1*n2)};
-var three = function(n1, n2, n3) {alert(n1*n2*n3)};
-var div = function(n1, n2) {alert(n1/n2)};
-var remainder = function(n1,n2) {alert(n1%n2)};
+$("h1").click(function() {
+  alert("Click this for relatable content ayyy lmao");
+});
 
-var number1 = parseInt(prompt("enter a number: "));
-var number2 = parseInt(prompt("enter another: "));
-var operator = prompt("add, subtract, multiply, or divide?")
+$("p").click(function() {
+  alert("Not as dank, not as meme.");
+});
 
-if (operator === "add") {
-  add(number1, number2);
-} else if (operator === "subtract") {
-  sub(number1, number2);
-} else if (operator === "multiply") {
-  mult(number1, number2);
-} else if (operator === "divide") {
- div(number1, number2);
-} else {
-  alert("You suck, try again scrub.")
+// $("img").click(function() {
+//   alert("astronomical")
+// });
+
+// document.getElementById("add").onclick = function {
+//   var text = documenet.getElementById("userinput").value;
+//   var li = "<li>" + text + "</li>";
+//   document.getElementById("list").appendChild(li);
+// }
+var titles  = [];
+var titleInput  = document.getElementById("title");
+var messageBox  = document.getElementById("display");
+
+shortcut.add("return",
+function() {
+  insert()
+},
+{ 'type':'keydown','propagate':true, 'target':document}
+);
+
+
+function insert () {
+    titles.push(titleInput.value);
+    clearAndShow();
+}
+
+function clearAndShow () {
+    titleInput.value = "";
+    messageBox.innerHTML = "";
+    messageBox.innerHTML += titles.join("<br/> ") + "<br/>";
 }
